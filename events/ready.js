2fb948eb3f28c419709517de1d5ff7053c480e4d
Thravalgur <thravalgur@gmail.com>
2024-12-31 18:47:38 +0100

    Status -> ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = {
 	async execute(client) {
 		// confirmer la présence en ligne du bot, mettre l'activité
 		console.log(`[${config.name}] : ${client.user.tag} présent sur ${client.guilds.cache.size} serveurs!`);
-		client.user.setPresence({ activities: [{ name: config.website.home, type: ActivityType.Watching }], status: 'online' });
+		client.user.setPresence({ activities: [{ name: "Les serveurs des Murmures Littéraires", type: ActivityType.Watching }], status: 'online' });
 		console.log('Ticket Bot connecté!');
 		// possibilité d'envoi automatique du panneau des tickets au démarrage
 		//		try {
