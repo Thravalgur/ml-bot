b0c062c2da0db697d97c56ee017f2d5a966f6cca
Thravalgur <thravalgur@gmail.com>
2025-01-04 12:57:04 +0100

    String lenght -> askInscrit Invalid string length Expected: expected.length <= 45

--- utils/askInscrit.js
+++ utils/askInscrit.js
@@ -16,7 +16,7 @@ module.exports = {
 		// créer un formulaire pour demander le titre
 		const modaltitle = new ModalBuilder()
 			.setCustomId('send-inscrit')
-			.setTitle('Quel manuscrit avez-vous inscrit à l\'édition en cours ?');
+			.setTitle('Quel manuscrit avez-vous inscrit ?');
 		// créer un champ dans le formulaire pour indiquer le titre
 		const titleInput = new TextInputBuilder ()
 			.setCustomId('titleInput')
